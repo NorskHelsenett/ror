fec2fbb485d832419a3b4d270496306ee6bbba4f
Håvard Elnan <havard.elnan@nhn.no>
2023-12-08 20:29:47 +0100

    add telemetry
    Signed-off-by: Håvard Elnan <havard.elnan@nhn.no>

---
+++ pkg/telemetry/trace/amqp_carrier.go
@@ -0,0 +1,45 @@
+package trace
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+)
+
+type AmqpHeadersCarrier map[string]interface{}
+
+func (a AmqpHeadersCarrier) Get(key string) string {
+	v, ok := a[key]
+	if !ok {
+		return ""
+	}
+	return v.(string)
+}
+
+func (a AmqpHeadersCarrier) Set(key string, value string) {
+	a[key] = value
+}
+
+func (a AmqpHeadersCarrier) Keys() []string {
+	i := 0
+	r := make([]string, len(a))
+
+	for k := range a {
+		r[i] = k
+		i++
+	}
+
+	return r
+}
+
+// InjectAMQPHeaders injects the tracing from the context into the header map
+func InjectAMQPHeaders(ctx context.Context) map[string]interface{} {
+	h := make(AmqpHeadersCarrier)
+	otel.GetTextMapPropagator().Inject(ctx, h)
+	return h
+}
+
+// ExtractAMQPHeaders extracts the tracing from the header and puts it into the context
+func ExtractAMQPHeaders(ctx context.Context, headers map[string]interface{}) context.Context {
+	return otel.GetTextMapPropagator().Extract(ctx, AmqpHeadersCarrier(headers))
+}
