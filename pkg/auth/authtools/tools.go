0c2e9c0e65226be4d6274aac5abedaba8cf49f9b
Håvard Elnan <havard.elnan@nhn.no>
2024-04-24 14:22:29 +0200

    no collisions please

--- pkg/auth/authtools/tools.go
+++ pkg/auth/authtools/tools.go
@@ -15,8 +15,8 @@ var (
 )
 
 func init() {
-	ServerConnectionHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{Name: "auth_server_connection_duration_seconds", Help: "Duration of server connection in seconds"}, []string{"provider", "domain", "host", "port", "status"})
-	UserLookupHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{Name: "auth_user_lookup_duration_seconds", Help: "Duration of user lookup in seconds"}, []string{"provider", "domain", "host", "status"})
+	ServerConnectionHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{Name: "auth_server_connection_duration_seconds", Help: "Duration of server connection in seconds"}, []string{"provider", "domain", "authserver", "port", "status"})
+	UserLookupHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{Name: "auth_user_lookup_duration_seconds", Help: "Duration of user lookup in seconds"}, []string{"provider", "domain", "authserver", "status"})
 	ServerReconnectCounter = promauto.NewCounterVec(prometheus.CounterOpts{Name: "auth_server_reconnects_total", Help: "Total number of server reconnects"}, []string{"provider", "domain"})
 }
 
