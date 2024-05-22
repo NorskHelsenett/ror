2d04bd213d3f7670baf7af753d37ded223b9a120
Håvard Elnan <havard.elnan@nhn.no>
2024-05-22 18:43:44 +0200

    move apiresponsens

---
+++ pkg/models/apiresponses/metricData.go
@@ -0,0 +1,10 @@
+package apiresponses
+
+import (
+	models "github.com/NorskHelsenett/ror/pkg/apicontracts"
+)
+
+type MetricData struct {
+	Total    models.Metrics `json:"total"`
+	Filtered models.Metrics `json:"filtered"`
+}
