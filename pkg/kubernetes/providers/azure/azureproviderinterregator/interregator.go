4739a3b3c5f8aa27ee90095c8e14aeb5b31915f8
Roger Westerbø <roger.westerbo@nhn.no>
2024-07-01 11:07:29 +0200

    fixing typeof

--- pkg/kubernetes/providers/azure/azureproviderinterregator/interregator.go
+++ pkg/kubernetes/providers/azure/azureproviderinterregator/interregator.go
@@ -1,6 +1,8 @@
 package azureproviderinterregator
 
 import (
+	"strings"
+
 	"github.com/NorskHelsenett/ror/pkg/models/providers"
 	v1 "k8s.io/api/core/v1"
 )
@@ -13,20 +15,24 @@ func NewInterregator() *Azuretypes {
 }
 
 func (t Azuretypes) IsTypeOf(nodes []v1.Node) bool {
-	return nodes[0].GetLabels()["kubernetes.azure.com/role"] != ""
+	return strings.Contains(strings.ToLower(nodes[0].Status.NodeInfo.OSImage), "talos")
 }
+
 func (t Azuretypes) GetProvider(nodes []v1.Node) providers.ProviderType {
 	if t.IsTypeOf(nodes) {
 		return providers.ProviderTypeAks
 	}
 	return providers.ProviderTypeUnknown
 }
+
 func (t Azuretypes) GetClusterName(nodes []v1.Node) string {
 	return nodes[0].GetLabels()["aks-cluster-name"]
 }
+
 func (t Azuretypes) GetWorkspace(nodes []v1.Node) string {
 	return "Azure"
 }
+
 func (t Azuretypes) GetDatacenter(nodes []v1.Node) string {
 	return nodes[0].GetLabels()["topology.kubernetes.io/region"]
 }
