2c1aeca3c809b075e072238cffafad5a38918c98
Roger Westerbø <roger@westerbo.no>
2024-10-15 10:11:48 +0200

    Fixing build sec error

--- cmd/api/controllers/notinuse/notinuse.go
+++ cmd/api/controllers/notinuse/notinuse.go
@@ -8,9 +8,6 @@ import (
 
 func NotInUse() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Printf("Hei %s du har kommet til et nummer som ikke er i bruk: %s%s\n", c.Request.Header["X-Forwarded-For"], c.Request.Host, c.Request.URL)
-		for key, header := range c.Request.Header {
-			fmt.Printf("%s: %s\n", key, header)
-		}
+		fmt.Printf("Hi %s you have reached a phone number not in use ... : %s%s\n", c.Request.Header["X-Forwarded-For"], c.Request.Host, c.Request.URL)
 	}
 }
