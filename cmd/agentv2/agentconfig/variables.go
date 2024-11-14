4e25db1dabaa0d152f4e6e3c793414c16b59908a
Håvard Elnan <havard.elnan@nhn.no>
2024-11-14 11:18:37 +0100

    add docs

--- cmd/agentv2/agentconfig/variables.go
+++ cmd/agentv2/agentconfig/variables.go
@@ -1,3 +1,23 @@
+//This package contains the variables used by the agentconfig package
+//It also contains the Init function which initializes the configuration
+//and sets the default values for the configuration variables
+//It also contains the IncreaseErrorCount and ResetErrorCount functions
+//which are used to increase the error count and reset the error count respectively
+//It also contains the GetRorVersion function which returns the RorVersion struct
+//which contains the version and commit of the agent
+//
+// The version and commit are set at compile time using the ldflags
+// The version and commit are set to the default  v1.1.0/FFFFF if not set at compile time
+//
+// The configuration variables are set to the default values if not set in the environment
+// The enviroment variables required are:
+// ROLE default value is ClusterAgent
+// HEALTH_ENDPOINT default value is :8100
+// POD_NAMESPACE default value is ror
+// API_KEY_SECRET default value is ror-apikey
+// API_KEY migt be provided by the user if a secret containg the api key is not present in the cluster
+//
+
 package agentconfig
 
 import (
