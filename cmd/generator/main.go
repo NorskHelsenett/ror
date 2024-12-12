9359f5986f2d0942155dd982183a213afb52a54f
kjetils-labs <77556155+kjetils-labs@users.noreply.github.com>
2024-12-12 13:50:12 +0100

    Cherry-picked feature/implement-backup

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -1,12 +1,16 @@
 // The Generator package provides a way to generate code for collecting,
 // transfering and saving resources in the agent and api.
-// It also provides functions to fetch the resources.
+// It also provides functions to fetc the resources.
 //
+<<<<<<< HEAD
 <<<<<<< HEAD
 //		go run build/generator/main.go
 =======
 //	go run cmd/generator/main.go
 >>>>>>> 550cbdd (Added resourceBackupJob types and generator stuff)
+=======
+//	go run build/generator/main.go
+>>>>>>> aaf2f15 (Update main.go)
 //
 //	  - pkg/apicontracts/apiresourcecontracts/resource_models_generated.go
 //	  - pkg/apicontracts/apiresourcecontracts/resource_models_methods_generated.go
