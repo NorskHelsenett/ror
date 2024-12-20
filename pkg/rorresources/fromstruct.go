a9c078b8e2fb9997acf6f6bdeeaad9b0f42ab97d
kjetil <kjetil.sigvartsen@nhn.no>
2024-12-20 15:32:48 +0100

    feature/ran-generator

--- pkg/rorresources/fromstruct.go
+++ pkg/rorresources/fromstruct.go
@@ -183,8 +183,7 @@ func NewResourceFromStruct(res Resource) *Resource {
 		r.SetCommonInterface(res.NetworkPolicyResource)
 
 	default:
-		rlog.Warn("Unknown resource kind", rlog.String("gvk", gvk.String()), rlog.String("kind", res.Kind), rlog.String("apiVersion", res.APIVersion))
-		return nil
+		rlog.Info("Unknown resource kind", rlog.String("gvk", gvk.String()), rlog.String("kind", res.Kind), rlog.String("apiVersion", res.APIVersion))
 	}
 	return r
 }
