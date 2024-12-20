9c02b727da7248440236da0185284a6aebc9c5d6
kjetil <kjetil.sigvartsen@nhn.no>
2024-12-20 14:47:14 +0100

    feature/Added-firewallrules-and-firewallpolicies

---
+++ pkg/rorresources/rortypes/resource_input_filter_firewallrule.go
@@ -0,0 +1,6 @@
+package rortypes
+
+// (r *ResourceFirewallRule) ApplyInputFilter Applies the input filter to the resource
+func (r *ResourceFirewallRule) ApplyInputFilter(cr *CommonResource) error {
+	return nil
+}
