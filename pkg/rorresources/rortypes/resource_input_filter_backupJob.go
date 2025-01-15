e74114f72ddd731d419243ba41bce9d6b614e6f7
Håvard Elnan <havard.elnan@nhn.no>
2025-01-15 12:30:42 +0100

    remove duplicate

--- pkg/rorresources/rortypes/resource_input_filter_backupJob.go
+++
@@ -1,6 +0,0 @@
-package rortypes
-
-// (r *resourceBackupJob) ApplyInputFilter Applies the input filter to the resource
-func (r *ResourceBackupJob) ApplyInputFilter(cr *CommonResource) error {
-	return nil
-}
