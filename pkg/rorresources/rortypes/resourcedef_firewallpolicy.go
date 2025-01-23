1c1e024916dd891adebf08afe9e46db17fdc59bb
kjetil <kjetil.sigvartsen@nhn.no>
2025-01-23 13:20:21 +0100

    added json tags, moved common parameters to it's own type

--- pkg/rorresources/rortypes/resourcedef_firewallpolicy.go
+++ pkg/rorresources/rortypes/resourcedef_firewallpolicy.go
@@ -3,97 +3,97 @@ package rortypes
 import "time"
 
 type ResourceFirewallPolicy struct {
-	Spec   ResourceFirewallPolicySpec
-	Status ResourceFirewallPolicyStatus
+	Spec   ResourceFirewallPolicySpec   `json:"spec"`
+	Status ResourceFirewallPolicyStatus `json:"status"`
 }
 
 type ResourceFirewallPolicySpec struct {
 
 	// Display name of the policy
-	Name string
+	Name string `json:"name"`
 
 	// Free text description
-	Description string
+	Description string `json:"description"`
 
 	// Unique id for the instance of a policy
-	Id string
+	Id string `json:"id"`
 
 	// Any applicable rules to this policy
-	Rules []ResourceFirewallRule
+	Rules []ResourceFirewallRule `json:"rules"`
 }
 
 type ResourceFirewallPolicyStatus struct {
 
 	// External references to this policy
-	ExternalIds map[string]string
+	ExternalIds map[string]string `json:"externalIds"`
 
-	CreatedAt      time.Time
-	CreatedBy      string
-	lastModifiedAt time.Time
-	LastModifiedBy string
-
+	ResourceCommonFieldsStatus
 	ResourceFirewallPolicySpec
 }
 
 type ResourceFirewallRule struct {
-	Spec   ResourceFirewallRuleSpec
-	Status ResourceFirewallRuleStatus
+	Spec   ResourceFirewallRuleSpec   `json:"spec"`
+	Status ResourceFirewallRuleStatus `json:"status"`
+}
+
+type ResourceCommonFieldsStatus struct {
+	CreatedAt      time.Time `json:"createdAt"`
+	CreatedBy      string    `json:"createdBy"`
+	LastModifiedAt time.Time `json:"lastModifiedAt"`
+	LastModifiedBy string    `json:"lastModifiedBy"`
 }
 
 type ResourceFirewallRuleSpec struct {
 
 	// Display name of the rule
-	Name string
+	Name string `json:"name"`
 
 	// Free text description
-	Description string
+	Description string `json:"description"`
 
 	// IPv4 (4), IPv6 (6), IPv4 && IPv6 (46)
-	IpType int
+	IpType int `json:"ipType"`
 
 	// ALLOW, DROP, DENY
-	Action string
+	Action string `json:"action"`
 
 	// The direction of the traffic flow
-	Direction string
+	Direction string `json:"direction"`
 
 	// TCP, UDP, ANY
-	Protocol string
+	Protocol string `json:"protocol"`
 
 	// The unique id for the instance of a rule
-	Id string
+	Id string `json:"id"`
 
 	// In what order is this rule applied, the lower the earlier
-	Sequence int
+	Sequence int `json:"sequence"`
 
 	// Ip addresses or group references
-	Sources []string
+	Sources []string `json:"sources"`
 
 	// Ip addresses or group references
-	Destinations []string
+	Destinations []string `json:"destinations"`
 
 	// Port number or service
-	Services []string
+	Services []string `json:"services"`
 
 	// Inclusion group that will have this rule applied to
-	Scope []string
+	Scope []string `json:"scope"`
 
 	// Whether this rule is active or deactivated
-	Disabled bool
+	Disabled bool `json:"disabled"`
 
 	// NSX, Fortigate, Checkpoint
-	Provider string
+	Provider string `json:"provider"`
 }
 
 type ResourceFirewallRuleStatus struct {
 
 	// External references to this rule
-	ExternalIds map[string]string
+	ExternalIds map[string]string `json:"externalIds"`
 
-	CreatedAt      time.Time
-	CreatedBy      string
-	lastModifiedAt time.Time
-	LastModifiedBy string
+	ResourceCommonFieldsStatus
 
 	ResourceFirewallRuleSpec
 }
