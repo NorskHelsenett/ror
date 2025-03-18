5b42b9b97a86572d909c3c62054626630632460e
jensl <jens.loe@nhn.no>
2025-03-18 08:01:48 +0100

    simplify functional options setup

--- pkg/rabbitmq/config.go
+++ pkg/rabbitmq/config.go
@@ -9,117 +9,77 @@ import (
 
 // ConsumerOption is an interface used for passing and applying configuration to
 // a Consumer.
-type ConsumerOption interface {
-	apply(*consumer) *consumer
-}
-
-type endpointOption string
+type ConsumerOption func(*consumer)
 
 func WithEndpoint(endpoint string) ConsumerOption {
-	return endpointOption(endpoint)
+	return func(c *consumer) {
+		c.endpoint = endpoint
+	}
 }
-
-func (o endpointOption) apply(c *consumer) *consumer {
-	c.endpoint = string(o)
-	return c
-}
-
-type nameOption string
 
 func WithName(name string) ConsumerOption {
-	return nameOption(name)
+	return func(c *consumer) {
+		c.consumerName = name
+	}
 }
-
-func (o nameOption) apply(c *consumer) *consumer {
-	c.consumerName = string(o)
-	return c
-}
-
-type queueNameOption string
 
 func WithQueueName(name string) ConsumerOption {
-	return queueNameOption(name)
+	return func(c *consumer) {
+		c.queueName = name
+	}
 }
-
-func (o queueNameOption) apply(c *consumer) *consumer {
-	c.queueName = string(o)
-	return c
-}
-
-type exchangeNameOption string
 
 func WithExchangeName(name string) ConsumerOption {
-	return exchangeNameOption(name)
+	return func(c *consumer) {
+		c.exchangeName = name
+	}
 }
-
-func (o exchangeNameOption) apply(c *consumer) *consumer {
-	c.exchangeName = string(o)
-	return c
-}
-
-type routingKeyOption string
 
 func WithRoutingKey(routingKey string) ConsumerOption {
-	return routingKeyOption(routingKey)
+	return func(c *consumer) {
+		c.routingKey = routingKey
+	}
 }
-
-func (o routingKeyOption) apply(c *consumer) *consumer {
-	c.routingKey = string(o)
-	return c
-}
-
-type argsOption amqp.Table
-
 func WithArgs(args amqp.Table) ConsumerOption {
-	return argsOption(args)
+	return func(c *consumer) {
+		c.args = args
+	}
 }
-
-func (o argsOption) apply(c *consumer) *consumer {
-	c.args = amqp.Table(o)
-	return c
-}
-
-type loggerOption slog.Logger
 
 func WithLogger(logger *slog.Logger) ConsumerOption {
-	return loggerOption(*logger)
+	return func(c *consumer) {
+		c.logger = logger
+	}
 }
 
-func (o loggerOption) apply(c *consumer) *consumer {
-	logger := slog.Logger(o)
-	c.logger = &logger
-	return c
-}
-
-func applyEnvOptions(c *consumer) *consumer {
+func applyEnvOptions(c *consumer) {
 	opts := getOptionsFromEnv()
 	for _, opt := range opts {
-		c = opt.apply(c)
+		opt(c)
 	}
-	return c
 }
 
 func getOptionsFromEnv() []ConsumerOption {
 	var opts []ConsumerOption
 	endpoint, ok := os.LookupEnv("ROR_RABBITMQ_ENDPOINT")
 	if ok {
-		opts = append(opts, endpointOption(endpoint))
+		opts = append(opts, WithEndpoint(endpoint))
 	}
 	consumerName, ok := os.LookupEnv("ROR_RABBITMQ_CONSUMER_NAME")
 	if ok {
-		opts = append(opts, nameOption(consumerName))
+		opts = append(opts, WithName(consumerName))
 	}
 	queueName, ok := os.LookupEnv("ROR_RABBITMQ_QUEUE_NAME")
 	if ok {
-		opts = append(opts, queueNameOption(queueName))
+		opts = append(opts, WithQueueName(queueName))
 	}
 	exchangeName, ok := os.LookupEnv("ROR_RABBITMQ_EXCHANGE_NAME")
 	if ok {
-		opts = append(opts, exchangeNameOption(exchangeName))
+		opts = append(opts, WithExchangeName(exchangeName))
 	}
 	routingKey, ok := os.LookupEnv("ROR_RABBITMQ_ROUTING_KEY")
 	if ok {
-		opts = append(opts, routingKeyOption(routingKey))
+		opts = append(opts, WithRoutingKey(routingKey))
 	}
 	return opts
 }
