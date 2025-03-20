4e412c0c4403de8c77643cde5dfdd1d547119010
jensl <jens.loe@nhn.no>
2025-03-20 10:07:00 +0100

    rewrite options pattern to use an option interface

--- pkg/rabbitmq/config.go
+++ pkg/rabbitmq/config.go
@@ -1,73 +1,103 @@
 package rabbitmq
 
 import (
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log/slog"
 	"os"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-// ConsumerOption is an interface used for passing and applying configuration to
-// a Consumer.
-type ConsumerOption func(*consumer)
+// option is an interface for passing configuration options to the structs in
+// this package.
+type option interface {
+	setEndpoint(string)
+	setConsumerName(string)
+	setQueueName(string)
+	setExchangeName(string)
+	setRoutingKey(string)
+	setArgs(amqp.Table)
+	setLogger(*slog.Logger)
+	setAuthenticator(Authenticator)
+	setExchangeType(string)
+}
 
-func WithEndpoint(endpoint string) ConsumerOption {
-	return func(c *consumer) {
-		c.endpoint = endpoint
+// Option is used in a functional options pattern to apply configuration options
+// to the consumer and producer structs provided in this package.
+type Option func(option)
+
+func WithConsumerName(name string) Option {
+	return func(o option) {
+		o.setConsumerName(name)
 	}
 }
 
-func WithName(name string) ConsumerOption {
-	return func(c *consumer) {
-		c.consumerName = name
+func WithEndpoint(endpoint string) Option {
+	return func(o option) {
+		o.setEndpoint(endpoint)
 	}
 }
 
-func WithQueueName(name string) ConsumerOption {
-	return func(c *consumer) {
-		c.queueName = name
+func WithQueueName(name string) Option {
+	return func(o option) {
+		o.setQueueName(name)
 	}
 }
 
-func WithExchangeName(name string) ConsumerOption {
-	return func(c *consumer) {
-		c.exchangeName = name
+func WithRoutingKey(key string) Option {
+	return func(o option) {
+		o.setRoutingKey(key)
 	}
 }
 
-func WithRoutingKey(routingKey string) ConsumerOption {
-	return func(c *consumer) {
-		c.routingKey = routingKey
-	}
-}
-func WithArgs(args amqp.Table) ConsumerOption {
-	return func(c *consumer) {
-		c.args = args
+func WithArgs(args amqp.Table) Option {
+	return func(o option) {
+		o.setArgs(args)
 	}
 }
 
-func WithLogger(logger *slog.Logger) ConsumerOption {
-	return func(c *consumer) {
-		c.logger = logger
+func WithLogger(logger *slog.Logger) Option {
+	return func(o option) {
+		o.setLogger(logger)
 	}
 }
 
-func applyEnvOptions(c *consumer) {
+func WithAuthenticator(authenticator Authenticator) Option {
+	return func(o option) {
+		o.setAuthenticator(authenticator)
+	}
+}
+
+func WithExchangeName(name string) Option {
+	return func(o option) {
+		o.setExchangeName(name)
+	}
+}
+
+func WithExchangeType(t string) Option {
+	return func(o option) {
+		o.setExchangeType(t)
+	}
+}
+
+// applyEnvOptions gets options from the environment and applies them to the
+// provided option interface.
+func applyEnvOptions(o option) {
 	opts := getOptionsFromEnv()
 	for _, opt := range opts {
-		opt(c)
+		opt(o)
 	}
 }
 
-func getOptionsFromEnv() []ConsumerOption {
-	var opts []ConsumerOption
+// getOptionsFromEnv gets various configuration options from environment
+// variables and returns an Option slice.
+func getOptionsFromEnv() []Option {
+	var opts []Option
 	endpoint, ok := os.LookupEnv("ROR_RABBITMQ_ENDPOINT")
 	if ok {
 		opts = append(opts, WithEndpoint(endpoint))
 	}
 	consumerName, ok := os.LookupEnv("ROR_RABBITMQ_CONSUMER_NAME")
 	if ok {
-		opts = append(opts, WithName(consumerName))
+		opts = append(opts, WithConsumerName(consumerName))
 	}
 	queueName, ok := os.LookupEnv("ROR_RABBITMQ_QUEUE_NAME")
 	if ok {
@@ -80,6 +110,10 @@ func getOptionsFromEnv() []ConsumerOption {
 	routingKey, ok := os.LookupEnv("ROR_RABBITMQ_ROUTING_KEY")
 	if ok {
 		opts = append(opts, WithRoutingKey(routingKey))
+	}
+	exchangeType, ok := os.LookupEnv("ROR_RABBITMQ_EXCHANGE_TYPE")
+	if ok {
+		opts = append(opts, WithExchangeType(exchangeType))
 	}
 	return opts
 }
