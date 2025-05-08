d8c9cf5755b0eefdc6fd2f8a0bc34269b8b816fc
Håvard Elnan <havard.elnan@nhn.no>
2025-05-08 15:37:03 +0200

    rename module

--- pkg/helpers/rorhealth/rorhealth.go
+++ pkg/helpers/rorhealth/rorhealth.go
@@ -3,34 +3,39 @@ package rorhealth
 import (
 	"context"
 
-	newhealth "github.com/dotse/go-health"
+	"github.com/dotse/go-health"
 )
 
-// WrappedChecker is a wrapper that adapts the Checker interface to conform to newhealth.Checker.
+// WrappedChecker is a wrapper that adapts the Checker interface to conform to health.Checker.
 type WrappedChecker struct {
 	Checker Checker
 }
 
-// CheckHealth implements the newhealth.Checker interface by adding context support.
-func (wc *WrappedChecker) CheckHealth(ctx context.Context) []newhealth.Check {
+// CheckHealth implements the health.Checker interface by adding context support.
+func (wc *WrappedChecker) CheckHealth(ctx context.Context) []health.Check {
 	return wc.Checker.CheckHealth()
 }
 
-// WrapChecker wraps a Checker to conform to the newhealth.Checker interface.
+// WrapChecker wraps a Checker to conform to the health.Checker interface.
 func WrapChecker(checker Checker) *WrappedChecker {
 	return &WrappedChecker{Checker: checker}
 }
 
 type Checker interface {
-	CheckHealth() (checks []newhealth.Check)
+	CheckHealth() (checks []health.Check)
 }
 
+// Deprecated: Use RegisterWithContext instead.
+// Register registers a health checker with the given name.
+// It wraps the provided Checker to conform to the health.Checker interface.
 func Register(name string, checker Checker) {
 	ctx := context.TODO()
 	wrappedChecker := WrapChecker(checker)
-	newhealth.Register(ctx, name, wrappedChecker)
+	health.Register(ctx, name, wrappedChecker)
 }
 
-func RegisterWithContext(ctx context.Context, name string, checker newhealth.Checker) newhealth.Registered {
-	return newhealth.Register(ctx, name, checker)
+// RegisterWithContext registers a health checker with the given name and context.
+// It wraps the provided Checker to conform to the health.Checker interface.
+func RegisterWithContext(ctx context.Context, name string, checker health.Checker) health.Registered {
+	return health.Register(ctx, name, checker)
 }
