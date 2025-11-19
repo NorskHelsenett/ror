3b901f49276159b696b1873b8cd5dc3bb6f80721
kjetil <kjetil.sigvartsen@nhn.no>
2025-11-19 08:16:45 +0100

    feature(acl): added constructors
    - Added constructor for NewAclV2ListItemAccess and NewAclV2ListItem.
    - Added constructor for commonly used permission sets in
      NewAclV2ListItemAccess.
    - Removed uneffective omitempty from json parameter Created in
      AclV2ListItem.

--- pkg/models/aclmodels/aclModelsV2.go
+++ pkg/models/aclmodels/aclModelsV2.go
@@ -34,8 +34,30 @@ type AclV2ListItem struct {
 	Access  AclV2ListItemAccess `json:"access" validate:"required"`                // v2 access model for ror api
 	//	Accessv2   []map[AccessType]bool    `json:"accessv2" validate:""`                      // v2 access model for ror api
 	Kubernetes AclV2ListItemKubernetes `json:"kubernetes" validate:""` // v2 access model for kubernetes
-	Created    time.Time               `json:"created,omitempty"`
+	Created    time.Time               `json:"created"`
 	IssuedBy   string                  `json:"issuedBy,omitempty" validate:"email"`
+}
+
+func NewAclV2ListItem(group string,
+	scope Acl2Scope,
+	subject Acl2Subject,
+	access AclV2ListItemAccess,
+	kubernetesLogon bool,
+	issuedBy string,
+) *AclV2ListItem {
+	return &AclV2ListItem{
+		Id:      "",
+		Version: 2,
+		Group:   group,
+		Scope:   scope,
+		Subject: subject,
+		Access:  access,
+		Kubernetes: AclV2ListItemKubernetes{
+			Logon: kubernetesLogon,
+		},
+		Created:  time.Now(),
+		IssuedBy: issuedBy,
+	}
 }
 
 // v2 access model for ror api
@@ -45,6 +67,47 @@ type AclV2ListItemAccess struct {
 	Update bool `json:"update" validate:"boolean"` // Update metadata of subject
 	Delete bool `json:"delete" validate:"boolean"` // Delete metadata of subject
 	Owner  bool `json:"owner" validate:"boolean"`  // Delete metadata of subject
+}
+
+// NewAclV2ListItemAccess construct a new AclV2ListItemAccess object.
+func NewAclV2ListItemAccess(read, create, update, delete, owner bool) AclV2ListItemAccess {
+	return AclV2ListItemAccess{
+		Read:   read,
+		Create: create,
+		Update: update,
+		Delete: delete,
+		Owner:  owner,
+	}
+}
+
+// NewAclV2ListItemAccessReadOnly gives you Read access.
+func NewAclV2ListItemAccessReadOnly() AclV2ListItemAccess {
+	return NewAclV2ListItemAccess(true, false, false, false, false)
+}
+
+// NewAclV2ListItemAccessCreateOnly gives you Read and Create  access.
+func NewAclV2ListItemAccessCreateOnly() AclV2ListItemAccess {
+	return NewAclV2ListItemAccess(true, true, false, false, false)
+}
+
+// NewAclV2ListItemAccessEditor gives you Read and Update access.
+func NewAclV2ListItemAccessEditor() AclV2ListItemAccess {
+	return NewAclV2ListItemAccess(true, false, true, false, false)
+}
+
+// NewAclV2ListItemAccessContributor gives you Read, Create, and Update access.
+func NewAclV2ListItemAccessContributor() AclV2ListItemAccess {
+	return NewAclV2ListItemAccess(true, true, true, false, false)
+}
+
+// NewAclV2ListItemAccessContributor gives you Read, Create, Update, and Delete access.
+func NewAclV2ListItemAccessOperator() AclV2ListItemAccess {
+	return NewAclV2ListItemAccess(true, true, true, true, false)
+}
+
+// NewAclV2ListItemAccessAll gives you Read, Create, Update, Delete, and Owner access.
+func NewAclV2ListItemAccessAll() AclV2ListItemAccess {
+	return NewAclV2ListItemAccess(true, true, true, true, true)
 }
 
 // v2 access model for kubernetes
