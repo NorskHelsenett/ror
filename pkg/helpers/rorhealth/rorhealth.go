b36ac2a208ab0ca96f1962f2143ab9c1153e1446
Håvard Elnan <havard.elnan@nhn.no>
2026-01-13 16:08:03 +0100

    Remove Gin handler and related imports from rorhealth package

--- pkg/helpers/rorhealth/rorhealth.go
+++ pkg/helpers/rorhealth/rorhealth.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/NorskHelsenett/ror-api/pkg/helpers/rorginerror"
 	"github.com/dotse/go-health"
-	"github.com/gin-gonic/gin"
 )
 
 const (
@@ -61,23 +59,4 @@ func Register(ctx context.Context, name string, checker Checker) health.Register
 
 func GetHttpHandler(w http.ResponseWriter, req *http.Request) {
 	health.HandleHTTP(w, req)
-}
-
-func GetGinHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		resp, err := health.CheckNow(c.Request.Context())
-		if err != nil {
-			rerr := rorginerror.NewRorGinError(http.StatusInternalServerError, "Unable to run health checks", err)
-			rerr.GinLogErrorAbort(c)
-			return
-		}
-		if resp.Status == health.StatusFail {
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		bytes, _ := resp.Status.MarshalJSON()
-		c.Data(http.StatusOK, "application/json", bytes)
-
-	}
 }
