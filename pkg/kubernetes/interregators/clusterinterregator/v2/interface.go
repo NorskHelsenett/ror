5acf2f5079bc2eef5bacef10347a8067ea533c35
Håvard Elnan <havard.elnan@nhn.no>
2026-01-19 23:28:24 +0100

    refactor(clusterinterregator): update import alias for meta package and clean up comments

--- pkg/kubernetes/interregators/clusterinterregator/v2/interface.go
+++ pkg/kubernetes/interregators/clusterinterregator/v2/interface.go
@@ -13,12 +13,11 @@ import (
 	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/unknown/unknownproviderinterregator/v2"
 	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/vitistack/vitistackinterregator/v2"
 	v1core "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-// IsTypeOf(nodes []v1.Node) bool
-
+// Ensure interfaces are implemented
 var (
 	_ interregatortypes.ClusterInterregator = (*vitistackinterregator.Vitistacktypes)(nil)
 	_ interregatortypes.ClusterInterregator = (*talosproviderinterregator.Talostypes)(nil)
@@ -45,7 +44,7 @@ var interregators = []interregatortypes.ClusterProviderInterregator{
 }
 
 func NewClusterInterregatorFromKubernetesClient(client *kubernetes.Clientset) interregatortypes.ClusterInterregator {
-	nodes, err := client.CoreV1().Nodes().List(context.TODO(), v1.ListOptions{})
+	nodes, err := client.CoreV1().Nodes().List(context.TODO(), v1meta.ListOptions{})
 	if err != nil {
 		return unknownproviderinterregator.NewInterregator()
 	}
