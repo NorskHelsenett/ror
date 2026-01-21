9427b4babe128243e42f6ff2e0c992af7560016f
Håvard Elnan <havard.elnan@nhn.no>
2026-01-21 15:12:31 +0100

    feat: replace stringhelper with RandomLowerAlphaNum for identifier generation

--- pkg/helpers/idhelper/id.go
+++ pkg/helpers/idhelper/id.go
@@ -1,9 +1,8 @@
 package idhelper
 
 import (
+	"crypto/rand"
 	"fmt"
-
-	"github.com/NorskHelsenett/ror/pkg/helpers/stringhelper"
 )
 
 // GetIdentifier returns a identifier from a name
@@ -12,7 +11,27 @@ import (
 //
 // - name: name to be turned into an identifier
 func GetIdentifier(name string) string {
-	idpostfix := stringhelper.RandomString(4, stringhelper.StringTypeClusterId)
+	idpostfix := RandomLowerAlphaNum(4)
 	identifier := fmt.Sprintf("%s-%s", name, idpostfix)
 	return identifier
+}
+
+// RandomLowerAlphaNum returns a random lowercase alphanumeric string with the requested length
+func RandomLowerAlphaNum(length int) string {
+	if length <= 0 {
+		return ""
+	}
+
+	const dictionary = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+	buf := make([]byte, length)
+	if _, err := rand.Read(buf); err != nil {
+		return ""
+	}
+
+	for i, b := range buf {
+		buf[i] = dictionary[int(b)%len(dictionary)]
+	}
+
+	return string(buf)
 }
