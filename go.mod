5461f12fd049416b7faeac8372381e7be14e4b91
Håvard Elnan <35875060+havardelnan@users.noreply.github.com>
2026-02-23 10:23:31 +0100

    chore: update Go version to 1.26 and refine workflow paths; adjust resource hash list types (#696)

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/NorskHelsenett/ror
 
-go 1.25.6
+go 1.26.0
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.13.1
--- pkg/clients/rorclient/transports/resttransport/v1/resources/hashes.go
+++ pkg/clients/rorclient/transports/resttransport/v1/resources/hashes.go
@@ -1,13 +1,13 @@
 package resources
 
 import (
-	"github.com/NorskHelsenett/ror/pkg/apicontracts/apiresourcecontracts"
 	"github.com/NorskHelsenett/ror/pkg/clients/rorclient/transports/resttransport/httpclient"
+	"github.com/NorskHelsenett/ror/pkg/helpers/resourcecache/resourcecachehashlist"
 	"github.com/NorskHelsenett/ror/pkg/models/aclmodels/rorresourceowner"
 )
 
-func (c *V1Client) GetHashList(ownerref rorresourceowner.RorResourceOwnerReference) (apiresourcecontracts.HashList, error) {
-	var hashList apiresourcecontracts.HashList
+func (c *V1Client) GetHashList(ownerref rorresourceowner.RorResourceOwnerReference) (resourcecachehashlist.HashList, error) {
+	var hashList resourcecachehashlist.HashList
 	params := []httpclient.HttpTransportClientParams{
 		{Key: httpclient.HttpTransportClientOptsQuery, Value: ownerref.GetQueryParams()},
 	}
