317ede881abfab7d2b4cbc2c2c8abfd7058994ac
Lucas <lucas.martin.thomaz@nhn.no>
2026-03-03 08:10:16 +0100

    Changed to include vulnerability metrics in resource

--- pkg/rorresources/rortypes/resourcedef_vm_vulnerabilities.go
+++ pkg/rorresources/rortypes/resourcedef_vm_vulnerabilities.go
@@ -1,7 +1,5 @@
 package rortypes
 
-type CVEId string
-
 type ResourceVirtualMachineVulnerabilityInfo struct {
 	Id       string                                    `json:"id"`
 	HostName string                                    `json:"hostName"`
@@ -13,10 +11,24 @@ type ResourceVirtualMachineVulnerabilityStatus struct {
 	HostSeverity        string  `json:"hostSeverity"`
 	Severity            string  `json:"severity"`
 	SeverityScore       float32 `json:"severityScore"`
-	CVEs                []CVEId `json:"cves"`
+	CVEs                []CVE   `json:"cves"`
 	LastCalculationTime int64   `json:"lastCalculationTime"`
 	LastReportTime      int64   `json:"lastReportTime"`
 }
 
 type ResourceVirtualMachineVulnerabilitySpec struct {
+}
+
+type CVE struct {
+	Id                 string              `json:"id"`
+	CVSS               string              `json:"csvss"`
+	Title              string              `json:"title"`
+	Description        string              `json:"description"`
+	References         []string            `json:"references"`
+	VulnerableVersions []VulnerableVersion `json:"vulnerableVersions"`
+}
+
+type VulnerableVersion struct {
+	Version     string `json:"version"`
+	PackageName string `json:"packageName"`
 }
