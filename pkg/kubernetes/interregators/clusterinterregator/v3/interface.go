bb8061ee7fc2ce8547424c3ed6c6b6c89ac8b902
Håvard Elnan <havard.elnan@nhn.no>
2026-06-01 21:56:16 +0200

    Feature: add additional provider detection functions to ClusterInterregator for enhanced compatibility

--- pkg/kubernetes/interregators/clusterinterregator/v3/interface.go
+++ pkg/kubernetes/interregators/clusterinterregator/v3/interface.go
@@ -3,6 +3,12 @@ package clusterinterregator
 import (
 	"github.com/NorskHelsenett/ror/pkg/kubernetes/interregators/factories/interregatorfactory/v3"
 	"github.com/NorskHelsenett/ror/pkg/kubernetes/interregators/interregatortypes/v3"
+	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/azure/azureproviderinterregator/v3"
+	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/gke/gkeproviderinterregator/v3"
+	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/k3d/k3dproviderinterregator/v3"
+	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/kind/kindproviderinterregator/v3"
+	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/talos/talosproviderinterregator/v3"
+	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/tanzu/tanzuproviderinterregator/v3"
 	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/unknown/unknownproviderinterregator/v3"
 	"github.com/NorskHelsenett/ror/pkg/kubernetes/providers/vitistack/vitistackinterregator/v3"
 	"k8s.io/client-go/kubernetes"
@@ -10,6 +16,12 @@ import (
 
 var interregators = []interregatortypes.ProviderDetectFunc{
 	vitistackinterregator.Detect,
+	tanzuproviderinterregator.Detect,
+	talosproviderinterregator.Detect,
+	kindproviderinterregator.Detect,
+	k3dproviderinterregator.Detect,
+	gkeproviderinterregator.Detect,
+	azureproviderinterregator.Detect,
 }
 
 func NewClusterInterregatorFromKubernetesClient(client *kubernetes.Clientset) interregatortypes.ClusterInterregator {
