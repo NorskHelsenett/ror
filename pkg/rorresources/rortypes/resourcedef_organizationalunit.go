efcca63f423d3f75aa51ef21390f78234349fb77
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-07-06 12:21:26 +0200

    added Interface and necessary functions

--- pkg/rorresources/rortypes/resourcedef_organizationalunit.go
+++ pkg/rorresources/rortypes/resourcedef_organizationalunit.go
@@ -1,5 +1,9 @@
 package rortypes
 
+import (
+	"context"
+)
+
 // In tags: "parent" is the ID of the parent organizational unit.
 // If empty, this is a top-level organizational unit and should be of type "organization".
 
@@ -26,3 +30,18 @@ const (
 	OrganizationalUnitTypeProject      OrganizationalUnitType = "project"
 	OrganizationalUnitTypeGroup        OrganizationalUnitType = "group"
 )
+
+// Configinterface represents the interface for resources of the type Config
+type OrganizationalUnitinterface interface {
+	Get() *ResourceOrganizationalUnit
+	ApplyOutputFilter(ctx context.Context, cr *CommonResource) error
+}
+
+func (r *ResourceOrganizationalUnit) Get() *ResourceOrganizationalUnit {
+	return r
+}
+
+func (r *ResourceOrganizationalUnit) ApplyOutputFilter(ctx context.Context, cr *CommonResource) error {
+	return nil
+
+}
