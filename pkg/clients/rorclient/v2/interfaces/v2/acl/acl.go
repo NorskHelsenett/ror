f3feed1b20f578bffc0ebc07da25fa9320c11254
dybsi <sigurd_od@hotmail.com>
2026-09-04 12:57:04 +0200

    added LookupByScopeSubject on v2 Rorclent

--- pkg/clients/rorclient/v2/interfaces/v2/acl/acl.go
+++ pkg/clients/rorclient/v2/interfaces/v2/acl/acl.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/NorskHelsenett/ror/pkg/models/aclmodels"
+	"github.com/NorskHelsenett/ror/pkg/models/aclmodels/aclscope"
 )
 
 type AclInterface interface {
@@ -12,4 +13,9 @@ type AclInterface interface {
 	// narrow the result to the given scopes and/or subjects (uids); pass nil for
 	// no filtering.
 	Lookup(ctx context.Context, access string, scopes []string, subjects []string) (*aclmodels.AclV3LookupResponse, error)
+
+	// LookupByScopeSubject resolves the access groups the caller has for the
+	// given scope+subject pair, using the V3 ACL backend. Unlike Lookup, scope
+	// and subject are required and identify a single resource.
+	LookupByScopeSubject(ctx context.Context, scope aclscope.Scope, subject aclscope.Subject) (*aclmodels.Acl3LookupByScopeSubjectResponse, error)
 }
